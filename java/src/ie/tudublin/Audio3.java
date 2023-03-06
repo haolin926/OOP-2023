d97f295d0d7a209a683fa41bf03a303203ba8125
haolin926 <c21325063@mytudublin.ie>
2023-03-06 11:10:00 +0000

    tutorial6

--- java/src/ie/tudublin/Audio3.java
+++ java/src/ie/tudublin/Audio3.java
@@ -1,5 +1,7 @@
 package ie.tudublin;
 
+import java.util.ArrayList;
+
 import ddf.minim.AudioBuffer;
 // import ddf.minim.AudioBuffer;
 import ddf.minim.AudioInput;
@@ -17,10 +19,11 @@ public class Audio3 extends PApplet{
     float lerpedY = 0;
 
     FFT fft;
+    ArrayList<Cube> cubes = new ArrayList<>();
 
     public void settings()
     {
-        size(1024, 1024);
+        size(1024, 1024,P3D);
     }
 
     public void setup()
@@ -37,19 +40,52 @@ public class Audio3 extends PApplet{
 
         fft = new FFT(width, 44100);
     }
-
+    float rot = 0.5f;
     float[] lerpedBuffer;
     public void draw()
     {
+
+        float total = 0;
+        for(int i = 0; i < ab.size();i++)
+        {
+            total += Math.abs(ab.get(i));
+        }
+        float average = total / (float) ab.size();
+
+        float lerpedAvergae =0;
+        lerpedAvergae = lerp(lerpedAvergae, average,0.1f);
+        float c = map(lerpedAvergae,0,0.5f,0,255);
         background(0);
         colorMode(HSB);
-        stroke(255);
-        
-        
+        lights();
+        stroke(c,255,255);
+        strokeWeight(5);
+        //fill(255);
+        noFill();
+        translate(width / 2, height / 2);
+        rotateY(rot);
+        rotateX(rot);
+        rot += map(lerpedAvergae,0,1.0f,0,1);
 
+        float boxSize = map(lerpedAvergae,0,0.5f,200,700);
+        box(boxSize);
 
 
         //println(map(5, 2, 10, 1000, 2000));
         //println(map1(5, 2, 10, 1000, 2000));
+    }
+    public void keyPressed()
+    {
+        if (key == ' ')
+        {
+            if(ap.isPlaying())
+            {
+                ap.pause();
+            }
+            else
+            {
+                ap.play();
+            }
+        }
     }
 }
---
+++ java/src/ie/tudublin/Cube.java
@@ -0,0 +1,19 @@
+package ie.tudublin;
+
+import processing.core.PApplet;
+
+public class Cube{
+    float color;
+    float size;
+    float rot;
+
+    public Cube()
+    {
+        
+    }
+
+    public void render(PApplet p)
+    {
+
+    }
+}
