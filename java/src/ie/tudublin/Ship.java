fe8cd4720bb02e25490a3f5dfd4dbbe04adbff07
haolin926 <c21325063@mytudublin.ie>
2023-03-20 12:39:08 +0000

    ships done

--- java/src/ie/tudublin/Ship.java
+++ java/src/ie/tudublin/Ship.java
@@ -5,9 +5,11 @@ import processing.core.PVector;
 
 public class Ship {
     private PVector pos;
-    private float rot;
+    private PVector forward;
     private int c;
     private float size;
+    private float halfSize;
+    private float rot;
     PApplet p;
 
     public PVector getPos() {
@@ -16,14 +18,6 @@ public class Ship {
 
     public void setPos(PVector pos) {
         this.pos = pos;
-    }
-
-    public float getRot() {
-        return rot;
-    }
-
-    public void setRot(float rot) {
-        this.rot = rot;
     }
 
     public int getC() {
@@ -45,14 +39,59 @@ public class Ship {
     public Ship(){
 
     }
+    public void move()
+    {
+        forward.x = PApplet.sin(rot);
+        forward.y = -PApplet.cos(rot);
+        if(p.keyPressed)
+        {
+            if(p.keyCode == PApplet.LEFT)
+            {
+                this.rot -= 0.1f;
+            }
+            if(p.keyCode == PApplet.RIGHT)
+            {
+                this.rot += 0.1f;
+            }
+            if(p.keyCode == PApplet.UP)
+            {
+                pos.x += forward.x;
+                pos.y += forward.y;
+            }
+            if(p.keyCode == PApplet.DOWN)
+            {
+                pos.x -= forward.x;
+                pos.y -= forward.y;
+            }
+
+        }
+    }
+
     
-    public Ship(float x,float y, float rot, int c, float size, PApplet p)
+    public Ship(float x,float y, int c, float size, PApplet p)
     {
         this.pos = new PVector(x,y);
-        this.rot = rot;
+        this.forward = new PVector(0,1);
         this.c = c;
         this.size = size;
         this.p = p;
+        this.halfSize = size/2;
+        this.rot = 0;
+    }
+
+    public void render()
+    {
+        p.pushMatrix();
+        p.stroke(c,255,255);
+        p.translate(pos.x,pos.y);
+        p.rotate(rot);
+        p.noFill();
+
+        p.line(-halfSize, halfSize, 0, -halfSize);
+        p.line(0, - halfSize, halfSize, halfSize);
+        p.line(- halfSize, halfSize, 0, 0);
+        p.line(0, 0, halfSize, halfSize);
+        p.popMatrix();
     }
 
 }
--- java/src/ie/tudublin/YASC.java
+++ java/src/ie/tudublin/YASC.java
@@ -1,24 +1,31 @@
 package ie.tudublin;
 
 import processing.core.PApplet;
+import processing.core.PVector;
 
 public class YASC extends PApplet
 {
-
+	Ship ship;
+	Ship ship2;
 	public void settings()
 	{
-		size(500, 500);
+		size(1024, 1024);
 	}
 
 	public void setup() {
-		
-		
+		colorMode(HSB);
+		background(0);
+		ship = new Ship(width/2,height/2,70,50,this);
+		ship2 = new Ship(width/2+100,height/2+100,20,70,this);
 	}
 
 	
-	
 	public void draw()
-	{	
-		
+	{
+		background(0);
+		ship.render();
+		ship.move();
+		ship2.render();
+		ship2.move();
 	}
 }
